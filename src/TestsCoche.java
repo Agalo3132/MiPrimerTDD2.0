092d01f9cd05dd6bef01d29d0035e5beec4a53a2
Alvaro <alvarosermar38@gmail.com>
2023-04-02 22:34:27 +0200

    Rama Refactorizada

--- src/TestsCoche.java
+++ src/TestsCoche.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 class TestsCoche {
 	@Test
-	public void test_al_crear_un_coche_su_velocidad_es_cero(){
+	public void test_al_crear_un_coche_su_velocidad_es_cero_Alvaro_Serrano(){
 		Coche nuevoCoche = new Coche();
         Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
